f2143af6a8f95db8bd9101eaed10d9a85bdeb397
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-07-06 18:36:26 +0500

    Add classes..

---
+++ find_athlete.py
@@ -0,0 +1,45 @@
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    first_name = sa.Column(sa.Text)
+    last_name = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    email = sa.Column(sa.Text)
+    birthdate = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
+
+class Athelete(Base):
+    __tablename__ = "athelete"
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    age = sa.Column(sa.Integer)
+    birthdate = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
+    name = sa.Column(sa.Text)
+    weight = sa.Column(sa.Integer)
+    gold_medals = sa.Column(sa.Integer)
+    silver_medals = sa.Column(sa.Integer)
+    bronze_medals = sa.Column(sa.Integer)
+    total_medals = sa.Column(sa.Integer)
+    sport = sa.Column(sa.Text)
+    country = sa.Column(sa.Text)
+
+def connect_db():
+    # создаём подключение к бд
+    engine = sa.create_engine(DB_PATH)
+    # создаём описанные таблицы
+    Base.metadata.create_all(engine)
+    # создаём фабрику сессий
+    Sessions = sessionmaker(engine)
+    # создаём сессию
+    session = Sessions()
+    return session
